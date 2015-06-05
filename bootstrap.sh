e1e22711ca5f7781395f857b92440038cba66e25
matt.freer <matt.freer@gmail.com>
2015-06-05 20:41:35 +0100

    fix(bootstrap): update protocol in the sources.list.docker.list
    A problem was observed with the docker provisioner failing during
    `apt-get update` on precise64 box. The problem is described in the
    following issue:
    
     * https://github.com/mitchellh/vagrant/issues/5748
    
    The issue was fixed by updating the url for `get.docker.io` to be
    `https` rather than `http` in the `sources.list.d/docker.list`.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sh -c "echo deb http://get.docker.io/ubuntu docker main\
+sh -c "echo deb https://get.docker.io/ubuntu docker main\
 > /etc/apt/sources.list.d/docker.list"
 apt-get -q -y update
 apt-get -q -y --force-yes install lxc-docker git python-pip
