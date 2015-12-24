4d3c63f20f8af378c3ce56e5e5d676348ab9b698
matt.freer <matt.freer@gmail.com>
2015-12-24 13:02:01 +0000

    feat(tmux): install version 2.0
    Install version 2.0 of tmux

--- recipes/tmux.sh
+++ recipes/tmux.sh
@@ -1,8 +1,10 @@
-apt-get -y build-dep tmux
-cd /usr/local/src
-wget http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.8/tmux-1.8.tar.gz
-tar -xf tmux-1.8.tar.gz
-cd tmux-1.8
-./configure --prefix /usr/local
-make
-make install
+apt-get build-dep tmux -y
+mkdir -p /usr/local/stow/tmux
+cd /usr/local/stow/tmux
+wget http://downloads.sourceforge.net/project/tmux/tmux/tmux-2.0/tmux-2.0.tar.gz
+tar -xzvf tmux-2.0.tar.gz
+mv tmux-2.0 tmux
+rm tmux-2.0.tar.gz
+cd /usr/local/stow/tmux/tmux
+./configure && make && sudo make install
+stow -d /usr/local/stow tmux
