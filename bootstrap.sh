690b6eb600b344610a51ab2ccf54e75e712771de
matt.freer <matt.freer@gmail.com>
2015-12-24 12:59:42 +0000

    feat(dependencies): install stow, wget and vim
    Install stow (from source), wget and vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,17 @@ apt-get -y install docker-engine
 sh -c 'curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 chmod +x /usr/local/bin/docker-compose
 
+#Install utils
+apt-get -y install wget vim
+
+#Install stow
+apt-get build-dep stow -y
+cd /usr/local/src
+wget http://ftp.heanet.ie/mirrors/gnu/stow/stow-latest.tar.gz
+tar -xzvf stow-latest.tar.gz
+mv stow-2.2.2 stow
+rm stow-latest.tar.gz
+cd stow
+./configure
+make && make install
+
