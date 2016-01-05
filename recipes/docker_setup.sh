cee240741f286e42bb9c3d2a8e6e8df4e9f82a86
matt.freer <matt.freer@gmail.com>
2016-01-05 10:38:29 +0000

    feat(docker_setup): add hosts entry for `private-registry.dev`
    A host entry is added for the `private-registry.dev` domain mapping to
    the `192.168.10.10` IP address.

--- recipes/docker_setup.sh
+++ recipes/docker_setup.sh
@@ -6,6 +6,7 @@ CERT_FILE="/certs/private-registry.dev/devdockerCA.crt"
 
 if [ -f $CERT_FILE ];
 then
+  sh -c 'echo "192.168.10.10 private-registry.dev" > /etc/hosts'
   mkdir /usr/local/share/ca-certificates/docker-dev-cert
   cp $CERT_FILE /usr/local/share/ca-certificates/docker-dev-cert
   update-ca-certificates
